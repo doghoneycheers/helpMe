55e30dc8557f607f8b34ff722f769a22d5c0d25d
kingworm <innovate57@gist.ac.kr>
2018-07-06 02:32:29 +0900

    hahaha
    dddd

--- app/src/main/java/com/example/q/helpme/SplashActivity.java
+++ app/src/main/java/com/example/q/helpme/SplashActivity.java
@@ -5,28 +5,23 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
+         // 로딩이 끝난후 이동할 Activity
+        int SPLASH_TIME=2000;
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/Chewy.ttf");
+        //setContentView(R.layout.activity_splash);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("state", "launch");
+        startActivity(intent);
+        finish();
 
-        TextView textView = findViewById(R.id.helper);
-        textView.setTypeface(typeFace);
-
-        Handler hd = new Handler();
-
-        hd.postDelayed(new splashHandler() , 2000); // 3초 후에 splashHandler 실행
-    }
-
-    private class splashHandler implements Runnable{
-        public void run() {
-            startActivity(new Intent(getApplication(), MainActivity.class)); // 로딩이 끝난후 이동할 Activity
-            SplashActivity.this.finish(); // 로딩페이지 Activity Stack에서 제거
-        }
     }
 
 
